4c56400951afc8981e10b8fe259111e814c5c586
KashanAdnan-Coder <computertechub0@gmail.com>
2024-07-30 16:07:46 +0500

    async handler added to auth controller

--- controllers/userCtrl.mjs
+++ controllers/userCtrl.mjs
@@ -1,20 +1,20 @@
 import User from '../models/userModel.mjs'
+import asyncHandler from "express-async-handler"
 
-const createUser = async (req, res) => {
-    const email = req.body.email
-    const findUser = await User.findOne({ email })
-    if (!findUser) {
-        // create a new user
-        const newUser = await User.create(req.body)
-        res.json(newUser)
-    } else {
-        // User Already Exist
-        res.json({
-            msg: "User Already Exists",
-            success: false
-        })
+const createUser = asyncHandler(
+    async (req, res) => {
+        const email = req.body.email
+        const findUser = await User.findOne({ email })
+        if (!findUser) {
+            // create a new user
+            const newUser = await User.create(req.body)
+            res.json(newUser)
+        } else {
+            // User Already Exist
+            throw new Error("User Already Exists")
+        }
     }
-}
+)
 
 export {
     createUser
