4be6c121313e3a60099a245455adc9f36ce9709d
KashanAdnan-Coder <computertechub0@gmail.com>
2024-07-30 16:05:35 +0500

    error handling complete

--- index.mjs
+++ index.mjs
@@ -4,16 +4,18 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/authRoutes.mjs"
 dotenv.config()
 import dbConnect from "./config/dbConnect.mjs"
+import { notFound, errorHandler } from "./middlewares/errorHandler.mjs"
 const PORT = process.env.PORT || 4000
+dbConnect()
 
 
 app.use(express.json())
-app.use("/api/user", authRoutes)
-app.use("/", (req, res) => {
-    res.send("Kashan")
-})
 
-dbConnect()
+app.use("/api/user", authRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(PORT, () => {
     console.log(`Server is running in Port ${PORT}`);
